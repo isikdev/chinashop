27171329b447a170e5df36f45e04b3fe841898d0
Iskandar <isikjandev@gmail.com>
2023-10-02 23:11:51 +0500

    100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,19 @@ $('.watch__card-wrapper').slick({
         }
     ]
 });
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    infinity: false
+});
