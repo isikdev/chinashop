42bf0ca44b942271ee6880f374ebec9b34744618
isikdev <isikjandev@gmail.com>
2023-10-03 00:20:40 +0500

    100%

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+$('.watch__card-wrapper').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }
+    ]
+});
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    infinity: false
+});
+$('.reviews__wrapper').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }
+    ]
+});
